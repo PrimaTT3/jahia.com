a20378a7043e931546d30d96752b3e9397fcc86b
Gautier Ben Aïm <48261497+GauBen@users.noreply.github.com>
2025-04-10 12:03:02 +0200

    feat: basic seo tags (#6)

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -3,21 +3,69 @@ import {
   buildModuleFileUrl,
   useServerContext,
 } from "@jahia/javascript-modules-library";
+import type { JCRNodeWrapper } from "org.jahia.services.content";
 import type { ReactNode } from "react";
 
 import "modern-normalize/modern-normalize.css";
 import "./global.css";
 
 /** Places `children` in an html page. */
-export const Layout = ({ title, children }: { title: string; children: ReactNode }) => {
-  const { currentResource } = useServerContext();
+export const Layout = ({
+  title,
+  description,
+  keywords,
+  openGraphImage,
+  children,
+}: {
+  title: string;
+  description?: string;
+  keywords?: string[];
+  openGraphImage?: JCRNodeWrapper;
+  children: ReactNode;
+}) => {
+  const { currentResource, renderContext } = useServerContext();
   const lang = currentResource.getLocale().getLanguage();
   return (
     <html lang={lang}>
       <head>
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <AddResources type="css" resources={buildModuleFileUrl("dist/server/style.css")} />
         <title>{title}</title>
+        <meta property="og:title" content={title} />
+        {description && (
+          <>
+            <meta name="description" content={description} />
+            <meta property="og:description" content={description} />
+          </>
+        )}
+        {keywords?.length && <meta name="keywords" content={keywords?.join(", ")} />}
+        {openGraphImage && (
+          <>
+            <meta
+              property="og:image"
+              content={openGraphImage.getAbsoluteUrl(renderContext.getRequest())}
+            />
+            {openGraphImage.hasProperty("jcr:title") && (
+              <meta
+                property="og:image:alt"
+                content={openGraphImage.getProperty("jcr:title").getString()}
+              />
+            )}
+            {openGraphImage.hasProperty("j:width") && (
+              <meta
+                property="og:image:width"
+                content={openGraphImage.getProperty("j:width").getString()}
+              />
+            )}
+            {openGraphImage.hasProperty("j:height") && (
+              <meta
+                property="og:image:height"
+                content={openGraphImage.getProperty("j:height").getString()}
+              />
+            )}
+          </>
+        )}
+        <AddResources type="css" resources={buildModuleFileUrl("dist/server/style.css")} />
       </head>
       <body>{children}</body>
     </html>
--- src/templates/Page/basic.server.tsx
+++ src/templates/Page/basic.server.tsx
@@ -1,5 +1,13 @@
 import { Area, jahiaComponent } from "@jahia/javascript-modules-library";
+import type { JCRNodeWrapper } from "org.jahia.services.content";
 import { Layout } from "../Layout.jsx";
+
+interface Props {
+  "jcr:title": string;
+  "jcr:description"?: string;
+  "seoKeywords"?: string[];
+  "openGraphImage"?: JCRNodeWrapper;
+}
 
 jahiaComponent(
   {
@@ -8,8 +16,18 @@ jahiaComponent(
     name: "basic",
     displayName: "Basic page",
   },
-  ({ "jcr:title": title }) => (
-    <Layout title={title}>
+  ({
+    "jcr:title": title,
+    "jcr:description": description,
+    "seoKeywords": keywords,
+    openGraphImage,
+  }: Props) => (
+    <Layout
+      title={title}
+      description={description}
+      keywords={keywords}
+      openGraphImage={openGraphImage}
+    >
       <Area name="main" />
     </Layout>
   ),
