45502ff3f15f3a4320f5991efaf5ed3dbde70329
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-04-22 14:50:40 +0200

    fix: make HeroCTA more resilient for broken data

--- src/views/Hero/HeroCTA.tsx
+++ src/views/Hero/HeroCTA.tsx
@@ -3,18 +3,22 @@ import type { JCRNodeWrapper } from "org.jahia.services.content";
 import { CTA } from "../../components/CTA.jsx";
 
 export type HeroCTAProps =
-  | { "ctaType": "internal"; "j:linknode": JCRNodeWrapper; "ctaLabel"?: string }
-  | { "ctaType": "external"; "j:url": string; "j:linkTitle": string; "ctaLabel"?: string };
+  | { "ctaType": "internal"; "j:linknode"?: JCRNodeWrapper; "ctaLabel"?: string }
+  | { "ctaType": "external"; "j:url"?: string; "j:linkTitle"?: string; "ctaLabel"?: string };
 
 export const HeroCTA = ({ cta }: { cta: HeroCTAProps }) => (
   <CTA
-    href={cta.ctaType === "internal" ? buildNodeUrl(cta["j:linknode"]) : cta["j:url"]}
+    href={
+      cta.ctaType === "internal"
+        ? cta["j:linknode"] && buildNodeUrl(cta["j:linknode"])
+        : cta["j:url"]
+    }
     title={cta.ctaType === "external" ? cta["j:linkTitle"] : undefined}
     icon
   >
     {cta.ctaLabel ||
       (cta.ctaType === "internal"
-        ? cta["j:linknode"].getPropertyAsString("jcr:title")
+        ? (cta["j:linknode"]?.getPropertyAsString("jcr:title") ?? "Internal link not defined")
         : cta["j:linkTitle"])}
   </CTA>
 );
