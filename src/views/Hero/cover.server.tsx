52410428fa5c491708a1ce87df804e717ee84399
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-05-28 15:45:36 +0200

    fix: remove duplicate Image component

--- src/views/Hero/cover.server.tsx
+++ src/views/Hero/cover.server.tsx
@@ -1,27 +1,9 @@
-import { buildNodeUrl, jahiaComponent } from "@jahia/javascript-modules-library";
+import { jahiaComponent } from "@jahia/javascript-modules-library";
 import classes from "./component.module.css";
 import type { Props } from "./types.js";
 import clsx from "clsx";
 import { MixinCTA } from "../../mixins/CTA/server.jsx";
-import type { JCRNodeWrapper } from "org.jahia.services.content";
-
-const BackgroundImage = ({ image }: { image: JCRNodeWrapper }) => {
-  const src = buildNodeUrl(image);
-  const alt = image.getPropertyAsString("jcr:title");
-  const width = image.getPropertyAsString("j:width");
-  const height = image.getPropertyAsString("j:height");
-
-  return (
-    <img
-      src={`${src}?w=400`}
-      srcSet={`${src}?w=400 400w, ${src}?w=800 800w, ${src}?w=1600 1600w, ${src}?w=2400 2400w`}
-      alt={alt}
-      width={width}
-      height={height}
-      className={classes.background}
-    />
-  );
-};
+import { Image } from "../../components/Image.jsx";
 
 jahiaComponent(
   {
@@ -31,7 +13,7 @@ jahiaComponent(
   },
   ({ theme, "jcr:title": title, subtitle, image, ...cta }: Props) => (
     <header className={classes.cover} data-theme={theme}>
-      {image && <BackgroundImage image={image} />}
+      {image && <Image image={image} fetchPriority="high" className={classes.background} />}
       <div className={clsx(classes.header, "_stack-4")}>
         <h1>{title || "Title not defined"}</h1>
         {subtitle && <p>{subtitle}</p>}
