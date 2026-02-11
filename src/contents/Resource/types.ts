7f8989f4bb21d5447c53a8e15add2b30bf97567f
Gautier Ben Aïm <benaim.gautier@gmail.com>
2026-02-11 10:17:36 +0100

    refactor!: remove resources and resource categories

--- src/contents/Resource/types.ts
+++
@@ -1,9 +0,0 @@
-import type { JCRNodeWrapper } from "org.jahia.services.content";
-
-export interface Props {
-  "jcr:title"?: string;
-  "jcr:description"?: string;
-  "kind"?: JCRNodeWrapper;
-  "description"?: string;
-  "form"?: string;
-}
--- src/contents/ResourceCategory/types.ts
+++
@@ -1,7 +0,0 @@
-import type { JCRNodeWrapper } from "org.jahia.services.content";
-
-export interface Props {
-  "jcr:title"?: string;
-  "subtitle"?: string;
-  "icon"?: JCRNodeWrapper;
-}
